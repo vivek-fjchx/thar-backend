ba67982954768c4db5e8860b6fe215a5f6038182
Vivek Pandey <vivekahaguru@gmail.com>
2025-11-21 21:18:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],   # For now allow all
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
