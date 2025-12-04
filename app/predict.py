a9fe2fe188a46dcf1fee8521da54720aab893191
Vivek Pandey <vivekahaguru@gmail.com>
2025-12-04 18:02:28 +0530

    Update predict.py addded open cv faster than PIL

--- app/predict.py
+++ app/predict.py
@@ -3,6 +3,7 @@ import traceback
 import onnxruntime as ort
 from PIL import Image, UnidentifiedImageError
 import numpy as np
+import cv2
 import gc
 
 
@@ -29,18 +30,20 @@ class Predictor:
         self.class_names = ["thar", "wrangler"]
 
         # ---------- LIGHTWEIGHT PREPROCESS (pure numpy) ----------
-        def preprocess(pil_img):
-            pil_img = pil_img.resize((224, 224))
-            img_np = np.array(pil_img).astype(np.float32)
+ def preprocess(pil_img):
+    # convert PIL → OpenCV
+    img_np = np.array(pil_img)
 
-            img_np = img_np / 255.0
-            img_np = (img_np - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
+    # resize fast
+    img_np = cv2.resize(img_np, (224, 224), interpolation=cv2.INTER_AREA)
 
-            # HWC → CHW
-            img_np = np.transpose(img_np, (2, 0, 1))
+    img_np = img_np.astype(np.float32) / 255.0
+    img_np = (img_np - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
 
-            # Add batch dimension
-            return img_np[np.newaxis, :, :, :]   # shape (1,3,224,224)
+    # HWC → CHW
+    img_np = np.transpose(img_np, (2, 0, 1))
+
+    return img_np[np.newaxis, :, :, :] # shape (1,3,224,224)
 
         self.preprocess = preprocess
 
